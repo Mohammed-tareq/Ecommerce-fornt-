1edff93307578baa6936a0ca5f0c498777d524c1
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-05-19 13:46:13 +0300

    admin

--- assets/firebase/addandget/dataBase.js
+++ assets/firebase/addandget/dataBase.js
@@ -1,5 +1,5 @@
 import {app, analytics} from "../dataconfig.js";
-import {getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
+import {getFirestore, collection, addDoc  } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
 
 
 const db = getFirestore(app);
@@ -8,6 +8,11 @@ let productCollection = collection(db, "products")
 //  =======================the  input product========================
 
 let productID = document.getElementById("productID");
+let brandName = document.getElementById("brandName");
+let policy = document.getElementById("policy");
+let size = document.getElementById("size");
+let oldPrice = document.getElementById("oldPrice");
+let badge = document.getElementById("badge");
 let categoryName = document.getElementById("categoryName");
 let productName = document.getElementById("productName");
 let priceProduct = document.getElementById("priceProduct");
@@ -28,39 +33,51 @@ addProductBtn.addEventListener("click", addProductDB);
 
 
 //========================================= add function =====================
+
 async function addProductDB(e) {
     e.preventDefault();
-    console.log("add product");
     try {
-        if (!productID.value || !categoryName.value || !productName.value ||
+        if ( !categoryName.value || !productName.value ||
             !priceProduct.value || !productCount.value || !productImg.value ||
             !descriptionProduct.value) {
             alert("Please fill in all fields");
             return;
         }
 
+
+
         let  productObj = {
             ID: productID.value,
+            brand:brandName.value,
+            policy:policy.value,
+            size:size.value,
+            oldPrice:oldPrice.value,
+            badge:badge.value,
             category: categoryName.value,
-            product: productName.value,
+            title: productName.value,
             price: priceProduct.value,
             Count: productCount.value,
             Img: productImg.value,
             description: descriptionProduct.value,
-            createdAt: new Date().toISOString(),
+            createdAt: new Date().toLocaleString(),
         };
-
         const docRef = await addDoc(productCollection, productObj);
 
-        console.log("data added successfully", docRef);
+        console.log("data added successfully", docRef.id);
 
-        productID.value = '';
         categoryName.value = '';
         productName.value = '';
         priceProduct.value = '';
         productCount.value = '';
         productImg.value = '';
         descriptionProduct.value = '';
+        productID.value = '';
+        brandName.value = '';
+        policy.value = '';
+        size.value = '';
+        oldPrice.value = '';
+        badge.value = '';
+
 
         console.log("Product added successfully!");
 
